06f3a6ac346c990ebc4c9cce26e714e701ce3cb9
meureuet <gmdgjfr@gmail.com>
2022-05-04 19:43:12 +0900

    카메라 설정
    - 쿼터뷰
    - 플레이어 추적

--- 3dQuarterView/Assets/Scripts/Player.cs
+++ 3dQuarterView/Assets/Scripts/Player.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int speed;
     float hAxis;
     float vAxis;
+    bool walkDown;
 
     Vector3 moveVector;
 
-    public int speed;
+    Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        // 자식의 Animator 가져오기
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 키 입력
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        walkDown = Input.GetButton("Walk");
 
         moveVector = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVector * speed * Time.deltaTime;
+        // 키 입력 시 이동
+        transform.position += moveVector * speed * (walkDown ? 0.5f : 1f) * Time.deltaTime;
+
+        // 키 입력 시 애니메이션
+        animator.SetBool("isRun", moveVector != Vector3.zero);
+        animator.SetBool("isWalk", walkDown);
+
+        // 키 입력 방향 바라보기
+        transform.LookAt(transform.position + moveVector);
+
+
     }
 }
